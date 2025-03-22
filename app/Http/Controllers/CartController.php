26825e737f8cae09cb8542552bea3cfc74676f24
Nouman-Shahid <noumanshahid2486@gmail.com>
2025-03-22 12:53:45 +0500

    Optimized function logic

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
+use App\Models\Course;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class CartController extends Controller
 {
-    public function addToCart($id) {}
+    public function addToCart($id)
+    {
+
+        $user = Auth::user();
+        $courses = Course::find($id)->first();
+        $user_detail = User::find($id)->first();
+
+        $cartItems = Cart::create([
+
+            'student_id' => $user->id,
+            'course_id' => $id,
+
+        ]);
+
+        return Inertia::render('Cart.jsx', [
+            'courses' => $courses,
+            'data' => $user_detail
+        ]);
+    }
 }
